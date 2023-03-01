3ad4a68cf979d32daa7362535959e637d78162a8
hardeylarning <hardeylarning@gmail.com>
2023-03-01 11:44:36 +0100

    register email has been added successfully

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,6 +1,7 @@
 import HttpException from "../exceptions/HttpException"
 import adminModel from "../models/admin.model";
 import { validateEmail } from "../utils/email-validator"
+import generateToken from "../utils/jwt/generate-token";
 import sendMail from "../utils/sendMail";
 
 export const registerEmail = async (req, res) => {
@@ -14,11 +15,18 @@ export const registerEmail = async (req, res) => {
                 from: process.env.clientEmail,
                 to: email,
                 subject: 'Admin account registration',
-                text: 
+                text: `Kindly follow this link in order to continue with your registration process as an admin!
+                <a href='http://localhost:8080/admin-registration-continuation?email=${email}&token=${generateToken(email)}'>sign up</a>`
               }
-              await sendMail()
+              await sendMail(mailOptions)
+              return res.json({
+                status: "success",
+                message: "A registration link has been successfully sent to your email, kindly continue your registration from there.",
+              });
         }
-        throw new HttpException(400,"Invaid Email!")
+        else {
+            throw new HttpException(400,"Invaid Email!")
+        }
         
     } catch (err) {
         next(err)
