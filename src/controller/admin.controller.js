067c70234824144d6eb3bae84fe60754d667b563
hardeylarning <hardeylarning@gmail.com>
2023-03-01 11:04:54 +0100

    added admin.controller.js

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,12 +1,11 @@
 import HttpException from "../exceptions/HttpException"
 
-const createAccount = async (req, res, next) => {
+export const registerEmail = async (req, res) => {
     try {
+        
             throw new HttpException(400,"Email address taken")
         // return res.status(200).send("create a stakepro account")
     } catch (err) {
         next(err)
     }
 }
-
-export { createAccount }
