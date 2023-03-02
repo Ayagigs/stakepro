a106ec57789f0ddec4421dab74d8050dc804bab2
hardeylarning <hardeylarning@gmail.com>
2023-03-02 18:17:07 +0100

    registered activateAdmin to the route

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerEmail, updateAdminRecord } from "../controller/admin.controller";
+import { activateAdmin, registerEmail, updateAdminRecord } from "../controller/admin.controller";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/admin-register-email", registerEmail)
 adminRouter.put("/admin-registration-continuation", updateAdminRecord)
+adminRouter.put("/admin-activation/:id", activateAdmin)
 
 
 export default adminRouter
