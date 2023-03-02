66fb60a85fb92afa39cf5b1e89c03faf41bc0bfd
JEDIBASIL <jedidiahbasil@gmail.com>
2023-03-02 22:46:01 +0100

    added proper validation for phone number

--- src/validator_schema/createAccountShema.js
+++ src/validator_schema/createAccountShema.js
@@ -1,10 +1,17 @@
 import Joi from "joi";
+import { PhoneNumberUtil } from "google-libphonenumber"
+
+
+const phoneUtil = PhoneNumberUtil.getInstance();
+import logger from "../utils/logger"
 
 const createAccountSchema = Joi.object({
     username: Joi.string().required().min(3),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    phoneNumber:Joi.number().required()
+    phoneNumber: Joi.string().pattern(/^\+?[1-9]\d{1,14}$/) // pattern for phone number validation
+        .required()
+
 })
 
 export default createAccountSchema
